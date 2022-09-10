2d7e0cadd4c93f5e3806abbb4fcb00926f50e8a1
Pankaj809 <67035842+Pankaj809@users.noreply.github.com>
2022-09-10 23:06:26 +0545

    Add files via upload

---
+++ spelling detector.py
@@ -0,0 +1,14 @@
+from lib2to3.pytree import convert
+from textblob import TextBlob
+def Convert(string):
+    pan=list(string.split())
+    return pan
+pan1= input("enter your word: ")
+words=Convert(pan1)
+corrected=[]
+for i in words:
+    corrected.append(TextBlob(i))
+print("Wrong words: ", words)
+print("Corrected words are: ")
+for i in corrected:
+    print(i.correct(), end=" ")
